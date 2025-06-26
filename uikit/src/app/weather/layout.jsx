98adbc6ec0f54f29afb18dfa4900268212ffa7bf
LeoPytho <108222261+LeoPytho@users.noreply.github.com>
2025-06-27 03:34:41 +0700

    Create layout.jsx

---
+++ uikit/src/app/weather/layout.jsx
@@ -0,0 +1,31 @@
+'use client';
+import PropTypes from 'prop-types';
+
+// @next
+import dynamic from 'next/dynamic';
+import { Footer7 } from '@/blocks/footer';
+
+// @project
+import useDataThemeMode from '@/hooks/useDataThemeMode';
+
+const ScrollFab = dynamic(() => import('@/components/ScrollFab'));
+const SectionsLayout = dynamic(() => import('@/views/sections/layout'));
+/***************************  LAYOUT - SECTIONS  ***************************/
+
+export default function Sections({ children }) {
+  useDataThemeMode();
+
+  return (
+    <SectionsLayout>
+       <>
+        {children}
+
+        {/* scroll to top section */}
+        <ScrollFab />
+       </>
+     <Footer7 />
+    </SectionsLayout>
+  );
+}
+
+Sections.propTypes = { children: PropTypes.any };
